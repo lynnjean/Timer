d366451d17e10a45b214b4aedd8abb7bc4301790
Jin Kim <54016390+lynnjean@users.noreply.github.com>
2025-07-28 16:37:41 +0900

    오류 해결

--- timer.py
+++ timer.py
@@ -86,7 +86,7 @@ with col_left:
         else:
             st.markdown('타이머',help="타이머가 대기중입니다!") 
     with status_col3:
-        st.markdon(f'{int(progress*100)%}')
+        st.markdon(f'{int(progress*100)}%')
 
 with col_right:
     pass
